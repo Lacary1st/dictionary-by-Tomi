71c55d06577a0ad1c8699ae480a9429a633efafe
henry <hdebecca@gmail.com>
2022-12-19 08:36:08 +0100

    adding files

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
- 
+ const input = document.getElementById('input');
+ const search = document.getElementById('search');
+
+ async function dictionaryAPI(search){
+    let apiURL = `https://api.dictionaryapi.dev/api/v2/entries/en/${search.value}`;
+    const response = await fetch(apiURL);
+    const data = await response.json();
+ }
