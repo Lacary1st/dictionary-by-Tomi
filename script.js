44fcb981895a855c3c9c6376a507db24af799718
Lacary <oluwatomilola22@gmail.com>
2022-12-31 22:03:44 +0100

    making progress

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
  const input = document.getElementById('input');
  const search = document.getElementById('search');
+//  const meaning = document.getElementById('meaning');
+ const phonetics = document.getElementById('phonetics');
 
- async function dictionaryAPI(search){
-    let apiURL = `https://api.dictionaryapi.dev/api/v2/entries/en/${search.value}`;
+
+ search.addEventListener( 'click',  e =>{
+   e.preventDefault();
+
+
+ 
+    let apiURL = https://od-api.oxforddictionaries.com/api/v2/entries/en/${search.value}`;
     const response = await fetch(apiURL);
-    const data = await response.json();
- }
+   //  const data = await response.json();
+   data
+ 
+}
